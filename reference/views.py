14ae8294f2033c764f3f32a7bba7c2c903d47b4b
Arfat6635 <arfat6635@gmail.com>
2021-12-29 14:44:39 +0530

    Added the js view

--- reference/views.py
+++ reference/views.py
@@ -4,4 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,"base.html")
+    return render(request,"index.html")
+
+    
+def javascript(request):
+    context={
+        "Topic":"Javascript",
+        "Topic_Message":"The Javascript cheat sheet is a one-page reference sheet for the javascript programming language.",
+        "Topic_Start":"Getting Started",
+    }
+    return render(request,"javascript.html",context)
